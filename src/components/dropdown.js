64216eb7ef9b0f52de9f38f292cfd7803c9ad730
venelinpetrov <vpetrov33tu@gmail.com>
2019-08-01 22:23:25 +0300

    Refactoring

---
+++ src/components/dropdown.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './dropdown.scss';
+
+class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            expanded: false,
+            value: {},
+            data: props.data,
+            selectedItem: {}
+        }
+    }
+
+    render() {
+        const dataTextField = this.props.dataTextField;
+
+        return (
+            <div className="x-dropdown">
+                <div className="x-dropdown__input" onClick={this.expandCollapse.bind(this)}>{this.state.value[dataTextField]}</div>
+                <div className={"x-dropdown__animation-container" + (this.state.expanded ? ' -expanded' : '')}>
+                    <div className="x-dropdown__data-container">
+                        <ul>
+                            {
+                                this.state.data.map(item => <li
+                                    key={item[dataTextField]}
+                                    className={item === this.state.selectedItem ? '-selected' : ''}
+                                    onClick={this.handleItemClick.bind(this, item)}>
+                                    {item[dataTextField]}
+                                </li>)
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    expandCollapse() {
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    }
+
+    handleItemClick(value) {
+        this.setState({
+            value,
+            selectedItem: value,
+            expanded: false
+        });
+    }
+}
+
+export default Dropdown;
