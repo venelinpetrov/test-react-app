396b59a790b713a1ae1e99c77a3cd8d4757f6239
Venelin Petrov <venelin.petrov@progress.com>
2019-08-01 18:49:45 +0300

    Rename file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Playground from './App';
+import Playground from './Playground';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Playground />, document.getElementById('root'));
